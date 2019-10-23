5e9207b84a000fea460a287269aece60393c3b53
polidog <polidogs@gmail.com>
2019-10-23 23:39:49 +0900

    Fix logger

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -6,12 +6,19 @@ namespace Polidog\SimpleApiBundle\EventListener;
 
 use Polidog\SimpleApiBundle\Exception\ErrorException;
 use Polidog\SimpleApiBundle\ResponseHandler\HandlerProviderInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class ExceptionListener implements EventSubscriberInterface
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
     /**
      * @var HandlerProviderInterface
      */
@@ -21,31 +28,37 @@ class ExceptionListener implements EventSubscriberInterface
      * ExceptionListener constructor.
      *
      * @param HandlerProviderInterface $provider
+     * @param LoggerInterface $logger
      */
-    public function __construct(HandlerProviderInterface $provider)
+    public function __construct(HandlerProviderInterface $provider, LoggerInterface $logger = null)
     {
+        $this->logger = $logger;
         $this->provider = $provider;
     }
 
     public function onKernelException(GetResponseForExceptionEvent $event): void
     {
+        $exception = $event->getException();
+        $this->logException($exception, sprintf('Uncaught PHP Exception %s: "%s" at %s line %s', \get_class($exception), $exception->getMessage(), $exception->getFile(), $exception->getLine()));
+
+
         $request = $event->getRequest();
         $annotation = $request->attributes->get('_simple_api_annotation');
         if (null === $annotation) {
             return;
         }
 
-        if ($event->getException() instanceof ErrorException) {
+        if ($exception instanceof ErrorException) {
             $parameters = [
-                'errors' => $event->getException()->getData(),
+                'errors' => $exception->getData(),
             ];
         } else {
             $parameters = [
-                'message' => $event->getException()->getMessage(),
+                'message' => $exception->getMessage(),
             ];
         }
 
-        $statusCode = 0 === $event->getException()->getCode() ? 500 : $event->getException()->getCode();
+        $statusCode = 0 === $exception->getCode() ? 500 : $exception->getCode();
 
         $newResponse = $this->provider->getHandler($annotation->getFormat())->handle($parameters);
         $newResponse->setStatusCode($statusCode);
@@ -57,5 +70,16 @@ class ExceptionListener implements EventSubscriberInterface
         return [
             KernelEvents::EXCEPTION => ['onKernelException'],
         ];
+    }
+
+    protected function logException(\Exception $exception, $message): void
+    {
+        if (null !== $this->logger) {
+            if (!$exception instanceof HttpExceptionInterface || $exception->getStatusCode() >= 500) {
+                $this->logger->critical($message, ['exception' => $exception]);
+            } else {
+                $this->logger->error($message, ['exception' => $exception]);
+            }
+        }
     }
 }
